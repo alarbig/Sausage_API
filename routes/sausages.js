b2ec14b4a82afffa0cefe4ad2b02fdfe11c91807
Persistent <agrlarbig@gmail.com>
2024-01-09 15:18:38 -0700

    Added routes, setup a DB. Still working on structure and what kinds of data I want to add. lots more to do!

--- routes/sausages.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const sausages = require('../data/sausages.json');
-
-router.get('/', (req, res) => {
-  res.json(sausages);
-});
-
-module.exports = router;
