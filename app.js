4382fdf126cb68abe92a5c65ad50243350f60787
Persistent <agrlarbig@gmail.com>
2024-01-11 13:54:40 -0700

    updated routes so they are now working with the DB. Next step is to add authentication via Tokens

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const sausageRoutes = require('./routes/sausages');
+// const express = require('express');
+// const cors = require('cors');
+// const sausageRoutes = require('./routes/sausages');
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.use('/api/sausages', sausageRoutes);
+// app.use('/api/sausages', sausageRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
